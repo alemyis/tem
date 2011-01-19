5367ea85a67f08deb7e9303becd36c907aa03f73
Alemeshet Alemu <alemyis@gmail.com>
2011-01-19 02:05:29 -0800

    add pagination

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Proverb::Application.initialize!
+
+WillPaginate::ViewHelpers.pagination_options[:prev_label] = '&larr;'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = '&rarr;'
