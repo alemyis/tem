2267d834faacf2765ea36c9369ad098c0b175c76
Alemeshet Alemu <alemyis@gmail.com>
2011-01-21 23:08:05 -0800

    1) Customized will_paginate navigation 2) CSS/HTML/image layout updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationHelper
 end
+
+
+class MyLinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
+  def prepare(collection, options, template)
+    super
+    @options[:container] = false
+  end
+
+  protected
+
+    def pagination
+      items = (1..total_pages).to_a
+      items.unshift(:previous_page) if @collection.previous_page
+      items.push(:next_page) if @collection.next_page
+      items
+    end
+
+    def page_number(page)
+      #unless page == current_page
+        #tag(:span, link(tag(:i, 'O'), page, :title => "Page #{page}", :class => page == current_page ? "current" : nil))
+      #  @template.image_submit_tag "/images/center.png", :title => "Page #{page}", :onclick => "javascript:window.location='#{@template.url_for :page => page}'"
+      #else
+        #tag(:span, tag(:i, 'O'), :title => "Page #{page}")
+      #  @template.image_submit_tag "/images/center.png", :title => "Page #{page}"
+      #end
+    end
+
+    def previous_page
+      @template.image_submit_tag "/images/left.png", :class => 'previous_page', :onclick => "javascript:window.location='#{@template.url_for :page => @collection.previous_page}'"
+    end
+
+    def next_page
+      @template.image_submit_tag "/images/right.png", :class => 'next_page', :onclick => "javascript:window.location='#{@template.url_for :page => @collection.next_page}'"
+    end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Proverb::Application.initialize!
 
-WillPaginate::ViewHelpers.pagination_options[:prev_label] = '&larr;'
-WillPaginate::ViewHelpers.pagination_options[:next_label] = '&rarr;'
+WillPaginate::ViewHelpers.pagination_options[:renderer] = 'MyLinkRenderer'
+WillPaginate::ViewHelpers.pagination_options[:prev_label] = ''
+WillPaginate::ViewHelpers.pagination_options[:next_label] = ''
