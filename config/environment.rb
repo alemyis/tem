6372a1c6776954abc6ed1a556dd88d2a0a020687
Alemeshet Alemu <alemyis@gmail.com>
2011-01-23 20:12:06 -0800

    revert navigation ui

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Proverb::Application.initialize!
 
-WillPaginate::ViewHelpers.pagination_options[:renderer] = 'MyLinkRenderer'
-WillPaginate::ViewHelpers.pagination_options[:prev_label] = ''
-WillPaginate::ViewHelpers.pagination_options[:next_label] = ''
+#WillPaginate::ViewHelpers.pagination_options[:renderer] = 'MyLinkRenderer'
+WillPaginate::ViewHelpers.pagination_options[:prev_label] = '&lArr;'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = '&rArr;'
